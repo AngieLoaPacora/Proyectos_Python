b76cbea93dde1417f2c3170b3af6b8bbe285896b
Angie <angiepacora.1423@gmail.com>
2025-12-27 17:10:47 -0500

    Agregando emojis al juego piedra-papel-tijera

--- Nivel1_1/1_1_007_Juego_pie_Pap_tij.py
+++ Nivel1_1/1_1_007_Juego_pie_Pap_tij.py
@@ -2,7 +2,11 @@ import random
 
 
 def jugar():
-    opciones = ["piedra", "papel", "tijera"]
+    opciones = {
+        "piedra": "🪨",
+        "papel": "📄",
+        "tijera": "✂️"}
+
     # todo El jugador eloga una opcion
     jugador = input("Elige:piedra, papel o tijera:").lower()
     # todo asegurarse de que el jugador elija una opcion valida
@@ -10,31 +14,33 @@ def jugar():
         print("Opcion No valida , elija piedra, papel o tijera")
         return
     # todo la computadora elige aleatorioamente una opcion
-    computadora = random.choice(opciones)
+    computadora = random.choice(list(opciones.keys()))
     # todo Mostrar las elecciones
-    print(f"Tu elegiste:{jugador}")
-    print(f"La Computadora eligió {computadora}")
+    print(f"Tu elegiste:{opciones[jugador]} ({jugador})")
+    print(f"La Computadora eligió {opciones[computadora]} ({computadora}")
     # todo determinar el resultado del juego
     if jugador == computadora:
-        print("Es un Empate!")
+        print("Es un Empate! 😐")
     elif (jugador == "piedra" and computadora == "tijera") or \
             (jugador == "papel" and computadora == "piedra") or \
             (jugador == "tijera" and computadora == "papel"):
-        print("Ganaste!")
+        print("Ganaste!🥳")
     else:
-        print("Perdiste!")
+        print("Perdiste! 🥲")
 
-#Bucle principal del juego para seguir jugando
+
+# Bucle principal del juego para seguir jugando
 def juego_piedra_papel_tijera():
-    print("Bienvenido al juego de Piedra, Papel o Tijera")
+    print("Bienvenido al juego de Piedra 🪨, Papel 📄 o Tijera ✂️!")
+
     while True:
         jugar()
-        #todo Preguntar si el jugador quiere jugar de nuevo
+        # todo Preguntar si el jugador quiere jugar de nuevo
         jugar_otra_vez = input("Quieres jugar otra vez? (SI/NO)").lower()
         if jugar_otra_vez != "si":
-            print("Gracias por Jugar")
+            print("Gracias por Jugar 👏🏽")
             break
-#todo Ejecutar el juego
+
+
+# todo Ejecutar el juego
 juego_piedra_papel_tijera()
-
-
